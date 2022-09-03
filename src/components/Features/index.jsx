6138cc6ed362f18e026ac2735e95559762ebba18
Aziz <aziz.khadjayev070@gmail.com>
2022-09-03 22:27:22 +0200

    APP-02 Add features

---
+++ src/components/Features/index.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import { Typography } from "@mui/material";
+
+function Features() {
+  return (
+    <div>
+      <StyledHeadline variant="h3">Start saving money</StyledHeadline>
+      <StyledSubHeadline variant="h5">
+        Find better deals across 100+ categories
+      </StyledSubHeadline>
+    </div>
+  );
+}
+
+const StyledHeadline = styled(Typography)`
+  && {
+    font-family: "Source Sans Pro", sans-serif;
+    font-weight: 700;
+    font-size: 32px;
+    line-height: 38.4px;
+    margin-bottom: 8px;
+  }
+`;
+const StyledSubHeadline = styled(Typography)`
+  && {
+    font-family: "Source Sans Pro", sans-serif;
+    font-size: 20px;
+    font-weight: 400;
+    line-height: 30px;
+    color: #6c7881;
+    margin-bottom: 16px;
+  }
+`;
+export default Features;
--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar";
 import Main from "../components/Main";
+import Features from "../components/Features";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Main />
+      <Features />
     </>
   );
 };
