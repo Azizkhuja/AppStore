66361c298ec52283103ff5928ed467fa10b9e774
Aziz <aziz.khadjayev070@gmail.com>
2022-09-04 21:35:14 +0200

    APP-02 Temporarly table

---
+++ src/components/Popular/PopularTable.jsx
@@ -0,0 +1,66 @@
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24),
+  createData("Ice cream sandwich", 237, 9.0, 37),
+];
+
+export default function PopularTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Rank</StyledTableCell>
+            <StyledTableCell align="right">App</StyledTableCell>
+            <StyledTableCell align="right">About</StyledTableCell>
+            <StyledTableCell align="right">Downloads</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {row.name}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.calories}</StyledTableCell>
+              <StyledTableCell align="right">{row.fat}</StyledTableCell>
+              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- src/components/Popular/index.jsx
+++ src/components/Popular/index.jsx
@@ -1,20 +1,31 @@
 import { Typography } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import PopularTable from "./PopularTable";
 
 const Popular = () => {
+  const matches = useMediaQuery("(min-width:900px)");
   return (
     <Grid container mt={6}>
-      <Grid item>
-        <Typography variant="h3" mb={4}>
+      <Grid item p={matches ? 0 : 2}>
+        <Typography variant={matches ? "h2" : "h4"} mb={3}>
           The most popular apps of the decade
         </Typography>
-        <Typography variant="body1">
+        <Typography variant="body1" mb={2}>
           Unsurprisingly, it seems that the apps that have ruled the Internet in
           the last decade have been social media apps, with Facebook emerging as
           the clear winner. Facebook does not only occupy the top two places on
           the podium, but the companies that it has acquired, WhatsApp and
           Instagram
         </Typography>
+      </Grid>
+      <Grid item xs={12} lg={6} p={2}>
+        Android
+        <PopularTable />
+      </Grid>
+      <Grid item xs={12} lg={6} p={2}>
+        Apple
+        <PopularTable />
       </Grid>
     </Grid>
   );
