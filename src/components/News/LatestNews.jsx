093f2a840cc68b0bcedbf803d54e7ae1f8991559
Aziz <aziz.khadjayev070@gmail.com>
2022-09-06 21:53:44 +0200

    APP-02 Add sections for news

--- src/components/News/LatestNews.jsx
+++ src/components/News/LatestNews.jsx
@@ -1,5 +1,19 @@
+import Grid from "@mui/material/Unstable_Grid2";
+
 const LatestNews = () => {
-  return <div>LatestNews</div>;
+  return (
+    <Grid container>
+      <Grid item xs={12} md={4}>
+        This is for 2 item is the row
+      </Grid>
+      <Grid item md={4}>
+        This is section for small 5 items
+      </Grid>
+      <Grid item md={4}>
+        This is for 2 item is the row
+      </Grid>
+    </Grid>
+  );
 };
 
 export default LatestNews;
