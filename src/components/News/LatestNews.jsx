a0649ebc3f87f1d4a35659b658d574806415102c
Aziz <aziz.khadjayev070@gmail.com>
2022-09-07 07:44:07 +0200

    APP-02 Add news card

--- src/components/News/LatestNews.jsx
+++ src/components/News/LatestNews.jsx
@@ -2,15 +2,18 @@ import Grid from "@mui/material/Unstable_Grid2";
 
 const LatestNews = () => {
   return (
-    <Grid container>
+    <Grid container spacing={1}>
       <Grid item xs={12} md={4}>
-        This is for 2 item is the row
+        This is for 2 item is the row This is for 2 item is the row This is for
+        2 item is the rows
       </Grid>
       <Grid item md={4}>
-        This is section for small 5 items
+        This is section for small 5 items This is section for small 5 items This
+        is section for small 5 items
       </Grid>
       <Grid item md={4}>
-        This is for 2 item is the row
+        This is for 2 item is the row This is for 2 item is the row This is for
+        2 item is the rows
       </Grid>
     </Grid>
   );
---
+++ src/components/News/NewCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewCard = () => {
+  return <div>NewCard</div>;
+};
+
+export default NewCard;
