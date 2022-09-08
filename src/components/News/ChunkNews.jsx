c8d789df5acf10824707fbffc293c05cf98d98ae
Aziz <aziz.khadjayev070@gmail.com>
2022-09-08 22:08:14 +0200

    APP-02 Style chunk card

--- src/components/News/ChunkNews.jsx
+++ src/components/News/ChunkNews.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
 
 const ChunkNews = () => {
-  return <div>ChunkNews</div>;
+  return (
+    <Card sx={{ display: "flex" }}>
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <CardContent sx={{ flex: "1 0 auto" }}></CardContent>
+      </Box>
+    </Card>
+  );
 };
 
 export default ChunkNews;
