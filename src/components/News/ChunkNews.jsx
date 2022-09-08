baec0763c6b585e40142d17ca0c2f14c82980fb9
Aziz <aziz.khadjayev070@gmail.com>
2022-09-08 22:08:46 +0200

    APP-02 Add data to chunk card components

--- src/components/News/ChunkNews.jsx
+++ src/components/News/ChunkNews.jsx
@@ -8,8 +8,25 @@ const ChunkNews = () => {
   return (
     <Card sx={{ display: "flex" }}>
       <Box sx={{ display: "flex", flexDirection: "column" }}>
-        <CardContent sx={{ flex: "1 0 auto" }}></CardContent>
+        <CardContent sx={{ flex: "1 0 auto" }}>
+          <Typography component="div" variant="h5">
+            Live From Space
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            color="text.secondary"
+            component="div"
+          >
+            Mac Miller
+          </Typography>
+        </CardContent>
       </Box>
+      <CardMedia
+        component="img"
+        sx={{ width: 151 }}
+        image="/static/images/cards/live-from-space.jpg"
+        alt="Live from space album cover"
+      />
     </Card>
   );
 };
