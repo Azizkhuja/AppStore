72a9cd7f19c5b8a8081403722b2689bcc0c10e6f
Aziz <aziz.khadjayev070@gmail.com>
2022-09-08 22:15:16 +0200

    APP-02 Display data of chunk component

--- src/components/News/ChunkNews.jsx
+++ src/components/News/ChunkNews.jsx
@@ -4,27 +4,32 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 
-const ChunkNews = () => {
+const ChunkNews = ({
+  chunkNewsTitle,
+  chunkNewsImg,
+  chunkNewsDate,
+  chunkNewsLink,
+}) => {
   return (
     <Card sx={{ display: "flex" }}>
       <Box sx={{ display: "flex", flexDirection: "column" }}>
         <CardContent sx={{ flex: "1 0 auto" }}>
           <Typography component="div" variant="h5">
-            Live From Space
+            {chunkNewsDate}
           </Typography>
           <Typography
             variant="subtitle1"
             color="text.secondary"
             component="div"
           >
-            Mac Miller
+            {chunkNewsTitle}
           </Typography>
         </CardContent>
       </Box>
       <CardMedia
         component="img"
         sx={{ width: 151 }}
-        image="/static/images/cards/live-from-space.jpg"
+        image={chunkNewsImg}
         alt="Live from space album cover"
       />
     </Card>
