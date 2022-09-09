89e141c030228de2f5053ab069aff1d2c85615ce
Aziz <aziz.khadjayev070@gmail.com>
2022-09-09 22:59:17 +0200

    Fix the mobile view

--- src/components/News/LatestNews.jsx
+++ src/components/News/LatestNews.jsx
@@ -1,8 +1,11 @@
 import Grid from "@mui/material/Unstable_Grid2";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
 import ChunkNews from "./ChunkNews";
 import TechCrunchNewsCard from "./TechCrunchNewsCard";
 
 const LatestNews = () => {
+  const matches = useMediaQuery("(min-width:900px)");
   return (
     <Grid container spacing={1}>
       <Grid item xs={12} md={4}>
@@ -33,9 +36,9 @@ const LatestNews = () => {
           newsLink={"newsLink4"}
         />
       </Grid>
-      <Grid item md={4}>
+      <Grid item md={4} sx={{ width: matches ? null : "100%" }}>
         <Grid container>
-          <Grid item xs={6}>
+          <Grid item xs={matches ? null : 6}>
             <ChunkNews
               chunkNewsTitle={"Chunk title"}
               chunkNewsImg={"Image"}
@@ -49,7 +52,7 @@ const LatestNews = () => {
               chunkNewsLink={"test"}
             />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={matches ? null : 6}>
             <ChunkNews
               chunkNewsTitle={"Chunk title"}
               chunkNewsImg={"Image"}
