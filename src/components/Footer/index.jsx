9fce8368c58bbe49a05289e8552e98d4512b1a4e
Aziz <aziz.khadjayev070@gmail.com>
2022-09-10 22:08:59 +0200

    APP-02 add footer links

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,24 @@
-import { Container, Box } from "@mui/material";
+import { Container, Box, Typography } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer>
+      <Container>
+        <Grid container spacing={1} sx={{ textAlign: "center" }}>
+          <Grid item xs={12}>
+            <Typography>Logo</Typography>
+            <ul>
+              <li>Link 1</li>
+              <li>Link 2</li>
+              <li>Link 3</li>
+              <li>Link 4</li>
+            </ul>
+          </Grid>
+        </Grid>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
