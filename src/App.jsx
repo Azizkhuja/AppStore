4b4a5a2d0df04e7f7139cd8a4926c2cbb25a8a09
Aziz <aziz.khadjayev070@gmail.com>
2022-09-10 11:34:24 +0200

    APP-02 Add footer section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Home from "./layout/Home";
+import Footer from "./components/Footer";
 
 function App() {
   const matches = useMediaQuery("(min-width:900px)");
@@ -12,6 +13,7 @@ function App() {
       <Container disableGutters={matches ? false : true}>
         <Home />
       </Container>
+      <Footer />
     </div>
   );
 }
