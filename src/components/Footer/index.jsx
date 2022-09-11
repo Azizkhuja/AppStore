a1fd0f70558fa4e04f47b92030babc6d13f286cb
Aziz <aziz.khadjayev070@gmail.com>
2022-09-11 13:39:50 +0200

    APP-02 Use styled component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import { Container, Box, Typography } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 // Social icons
@@ -8,29 +9,43 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Footer = () => {
   return (
-    <footer>
+    <FooterContainer>
       <Container>
         <Grid container spacing={1} sx={{ textAlign: "center" }}>
           <Grid item xs={12}>
             <Typography>Logo</Typography>
-            <ul>
-              <li>Link 1</li>
-              <li>Link 2</li>
-              <li>Link 3</li>
-              <li>Link 4</li>
-            </ul>
-            <ul>
-              <li>Social icons</li>
-              <li>Social icons</li>
-              <li>Social icons</li>
-              <li>Social icons</li>
-            </ul>
+            <OrderLists>
+              <OrderList>OrderListnk 1</OrderList>
+              <OrderList>OrderListnk 2</OrderList>
+              <OrderList>OrderListnk 3</OrderList>
+              <OrderList>OrderListnk 4</OrderList>
+            </OrderLists>
+            <OrderLists>
+              <OrderList>Social icons</OrderList>
+              <OrderList>Social icons</OrderList>
+              <OrderList>Social icons</OrderList>
+              <OrderList>Social icons</OrderList>
+            </OrderLists>
             <Typography> &copy; 2022 AppStore. All rights reserved</Typography>
           </Grid>
         </Grid>
       </Container>
-    </footer>
+    </FooterContainer>
   );
 };
+
+const FooterContainer = styled.footer`
+  background: #101b25;
+  color: #fff;
+  padding-top: 30px;
+  padding-bottom: 30px;
+`;
+const OrderLists = styled.ul`
+  display: flex;
+  justify-content: center;
+`;
+const OrderList = styled.li`
+  margin: 10px;
+`;
 
 export default Footer;
