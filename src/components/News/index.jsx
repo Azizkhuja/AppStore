c6ec3970ec7bb14c76b00c8a4464e6c47ffc7060
Aziz <aziz.khadjayev070@gmail.com>
2022-09-12 07:58:34 +0200

    APP-02 Add margin bottom to news card container

--- src/components/News/index.jsx
+++ src/components/News/index.jsx
@@ -4,9 +4,10 @@ import Grid from "@mui/material/Unstable_Grid2";
 import useMediaQuery from "@mui/material/useMediaQuery";
 
 const News = () => {
+  const small = useMediaQuery("(min-width:600px)");
   const medium = useMediaQuery("(min-width:900px)");
   return (
-    <Grid px={2}>
+    <Grid px={2} mb={small ? 3 : 1}>
       <Typography variant={medium ? "h5" : "h6"} my={3}>
         Latest guides, comparisons and news about apps
       </Typography>
