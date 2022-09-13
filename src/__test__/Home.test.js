02d934365d0a2bb4180507c983e7763a89563cd7
Aziz <aziz.khadjayev070@gmail.com>
2022-09-13 21:12:00 +0200

    APP-03 test header part

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -3,4 +3,10 @@ import "@testing-library/jest-dom";
 
 import Navbar from "../components/Navbar";
 
-describe("Should test Home page and nested components", () => {});
+describe("Should test Home page and nested components", () => {
+  it("should get Navbar title", () => {
+    render(<Navbar />);
+    const elTest = screen.getByText(/app store/i);
+    expect(elTest).toBeInTheDocument();
+  });
+});
