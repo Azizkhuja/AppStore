e1de493aa6d51fecc86e4d88f17075fb87aa70e3
Aziz <aziz.khadjayev070@gmail.com>
2022-09-13 20:57:55 +0200

    APP-02 Add testing condition

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -1,1 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
+import Navbar from "../components/Navbar";
+
+it("test", () => {
+  render(<Navbar />);
+  const elTest = screen.getByText(/App Store/i);
+  expect(elTest).toBeInTheDocument();
+});
