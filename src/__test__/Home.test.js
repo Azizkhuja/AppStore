8d5124e32bd9a808f8217903e4ea0e438a474ba4
Aziz <aziz.khadjayev070@gmail.com>
2022-09-13 21:15:05 +0200

    APP-03 Test main headline

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -2,11 +2,18 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 import Navbar from "../components/Navbar";
+import Hero from "../components/Main/Hero";
 
 describe("Should test Home page and nested components", () => {
   it("should get Navbar title", () => {
     render(<Navbar />);
     const elTest = screen.getByText(/app store/i);
+    expect(elTest).toBeInTheDocument();
+  });
+
+  it("should get main page of headline", () => {
+    render(<Hero />);
+    const elTest = screen.getByText(/get single application statistics/i);
     expect(elTest).toBeInTheDocument();
   });
 });
