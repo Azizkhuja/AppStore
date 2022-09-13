cf2b718d92bcf5b5f94b6e3221fa618458b6dbc8
Aziz <aziz.khadjayev070@gmail.com>
2022-09-13 08:04:48 +0200

    APP-02 Add link to chunk news

--- src/components/News/ChunkNews.jsx
+++ src/components/News/ChunkNews.jsx
@@ -1,6 +1,14 @@
 import useMediaQuery from "@mui/material/useMediaQuery";
+import { Link } from "react-router-dom";
 
-import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 
 const ChunkNews = ({
   chunkNewsTitle,
@@ -10,31 +18,37 @@ const ChunkNews = ({
 }) => {
   const smallMatches = useMediaQuery("(min-width:600px)");
   return (
-    <Card
-      sx={{
-        display: "flex",
-        marginBottom: 3,
-        justifyContent: smallMatches ? null : "space-between",
-        alignItems: smallMatches ? null : "center",
-        maxHeight: 124,
-        minHeight: 120,
-      }}
-    >
-      <Box sx={{ display: "flex", flexDirection: "column" }}>
-        <CardContent sx={{ flex: "1 0 auto" }}>
-          <Typography variant="subtitle2">{chunkNewsTitle}</Typography>
-          <Typography variant="caption" color="text.secondary" component="div">
-            {chunkNewsDate}
-          </Typography>
-        </CardContent>
-      </Box>
-      <CardMedia
-        component="img"
-        sx={{ width: 140 }}
-        image={chunkNewsImg}
-        alt={chunkNewsTitle}
-      />
-    </Card>
+    <CardActionArea href={chunkNewsLink} target="_blank">
+      <Card
+        sx={{
+          display: "flex",
+          marginBottom: 3,
+          justifyContent: smallMatches ? null : "space-between",
+          alignItems: smallMatches ? null : "center",
+          maxHeight: 124,
+          minHeight: 120,
+        }}
+      >
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <CardContent sx={{ flex: "1 0 auto" }}>
+            <Typography variant="subtitle2">{chunkNewsTitle}</Typography>
+            <Typography
+              variant="caption"
+              color="text.secondary"
+              component="div"
+            >
+              {chunkNewsDate}
+            </Typography>
+          </CardContent>
+        </Box>
+        <CardMedia
+          component="img"
+          sx={{ width: 140 }}
+          image={chunkNewsImg}
+          alt={chunkNewsTitle}
+        />
+      </Card>
+    </CardActionArea>
   );
 };
 
