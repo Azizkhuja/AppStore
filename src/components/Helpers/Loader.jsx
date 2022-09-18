a05adafb5d291d9082ef94a1e7e70865c5f89b0c
Aziz <aziz.khadjayev070@gmail.com>
2022-09-18 12:43:22 +0200

    APP-03 Add loader

--- src/components/Helpers/Loader.jsx
+++ src/components/Helpers/Loader.jsx
@@ -3,6 +3,8 @@ import LinearProgress from "@mui/material/LinearProgress";
 
 export default function Loader() {
   return (
-    
+    <Box sx={{ width: "100%" }}>
+      <LinearProgress />
+    </Box>
   );
 }
