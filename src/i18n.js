1dad08b1e623133d03819e0e505af8d0a81a39e2
Aziz <aziz.khadjayev070@gmail.com>
2022-09-20 23:18:38 +0200

    APP-03 Import language detector and backend

--- src/i18n.js
+++ src/i18n.js
@@ -1,2 +1,4 @@
 import i18next from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
