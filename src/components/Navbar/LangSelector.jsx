68f910a9dca0d931e0815b3503429f0d8ff6a27f
Aziz <aziz.khadjayev070@gmail.com>
2022-09-25 00:23:00 +0200

    APP-03 Bring to login to this component

--- src/components/Navbar/LangSelector.jsx
+++ src/components/Navbar/LangSelector.jsx
@@ -1,6 +1,25 @@
 import { useEffect } from "react";
 import i18next from "i18next";
 
-const LangSelector = () => {};
+const LangSelector = () => {
+  useEffect(() => {
+    if (localStorage.getItem("i18nextLng")?.length > 2) {
+      i18next.changeLanguage("en");
+    }
+  }, []);
+
+  const handleLanguageChange = (e) => {
+    i18next.changeLanguage(e.target.value);
+  };
+  return (
+    <select
+      value={localStorage.getItem("i18nextLng")}
+      onChange={handleLanguageChange}
+    >
+      <option value="us">🇺🇸 US</option>
+      <option value="es">🇪🇸 ES</option>
+    </select>
+  );
+};
 
 export default LangSelector;
