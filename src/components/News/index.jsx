aeeb4a927bca69082dfbdaf68c65bc7fd04486bc
Aziz <aziz.khadjayev070@gmail.com>
2022-09-24 21:58:46 +0200

    APP-03 Add transaltion for news

--- src/components/News/index.jsx
+++ src/components/News/index.jsx
@@ -2,14 +2,16 @@ import LatestNews from "./LatestNews";
 import { Typography } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTranslation } from "react-i18next";
 
 const News = () => {
   const small = useMediaQuery("(min-width:600px)");
   const medium = useMediaQuery("(min-width:900px)");
+  const { t } = useTranslation();
   return (
     <Grid px={2} mb={small ? 3 : 1}>
       <Typography variant={medium ? "h5" : "h6"} my={3}>
-        Latest guides, comparisons and news about apps
+        {t("news.title")}
       </Typography>
       <LatestNews />
     </Grid>
